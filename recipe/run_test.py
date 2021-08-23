485f7553715ba05014d515c68a97ff563fecc03c
Carlo Dri <carlo.dri@cpi-eng.it>
2021-08-23 09:03:55 +0200

    fix test image download in test

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,15 +1,16 @@
 from turbojpeg import TurboJPEG
 import requests
 
-url = "https://upload.wikimedia.org/wikipedia/commons/9/98/03-bryone-dioique.jpg"
+url = "https://raw.githubusercontent.com/libjpeg-turbo/libjpeg-turbo/main/testimages/testorig.jpg"
 r = requests.get(url, allow_redirects=True)
+r.raise_for_status()
 
 jpeg = TurboJPEG()
 
-with open("03-bryone-dioique.jpg", "wb") as test_file:
+with open("testorig.jpg", "wb") as test_file:
     test_file.write(r.content)
 
-with open("03-bryone-dioique.jpg", "rb") as in_file:
+with open("testorig.jpg", "rb") as in_file:
     bgr_array = jpeg.decode(in_file.read())
 
 with open("output.jpg", "wb") as out_file:
